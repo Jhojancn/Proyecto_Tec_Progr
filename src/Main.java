633e29983d55c75ae7d7dd40bd0ccb637f04d589
Alexander <anderalexanderaguirretejada@gmail.com>
2026-09-03 10:34:01 -0500

    Impresion de mensajes de prueba por consola, sin cambios significativos por la etapa actual del proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Prueba-1");
+        System.out.println("Prueba-2");
+        System.out.println("Prueba-3");
     }
 }
