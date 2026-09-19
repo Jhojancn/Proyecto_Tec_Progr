b2229b23d22610ae60078e03ecafa83c9c5fe4b0
Alexander <anderalexanderaguirretejada@gmail.com>
2026-09-19 11:09:39 -0500

    Regorganizacion de carpetas y cambio de formato de proyecto para compatibilidad con netbeans. Divicion de carpetas para casos y proyecto final

---
+++ Proyecto/Proyecto_final_Tecnicas_POO/src/proyecto_final_tecnicas_poo/Proyecto_final_Tecnicas_POO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_final_tecnicas_poo;
+
+/**
+ *
+ * @author ander
+ */
+public class Proyecto_final_Tecnicas_POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Este es el proyecto para la T3 y examen final, todo lo relacionado se trabaja aqui");
+    }
+    
+}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Prueba-1");
-        System.out.println("Prueba-2");
-        System.out.println("Prueba-3");
-        System.out.println("Práctica Semana 4");
-    }
-}
